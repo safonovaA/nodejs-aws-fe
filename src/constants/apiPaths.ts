3544e1dd4fa194323a531309f9ab791ceeece37b
Анна Сафонова <you@example.com>
2020-11-26 18:31:04 +0300

    Fix api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://xu1snc0rg3.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://vkv07g1v93.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://1yw3msxv73.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
