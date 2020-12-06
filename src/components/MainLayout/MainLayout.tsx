c2b9b77aef6ff6b5e7211b7442e05de6ae053dab
Анна Сафонова <you@example.com>
2020-12-06 21:57:44 +0300

    Change api path

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -8,9 +8,9 @@ import Header from "components/MainLayout/components/Header";
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © Comics Book Store'}
+      {'Copyright ©'}
       <Link color="inherit" href="https://material-ui.com/">
-        My Store
+      Comics Book Store
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,8 @@ const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://xu1snc0rg3.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://1yw3msxv73.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://1yw3msxv73.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://xw5ne0mhpa.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
